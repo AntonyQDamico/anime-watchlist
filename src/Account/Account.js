ecf65de6a9ae65abb1a1dae94f5a13b21c860de9
AntonyQDamico <AntonyQDamico@gmail.com>
2021-12-21 23:00:29 -0600

    fixed navbar and updated login look

--- src/Account/Account.js
+++ src/Account/Account.js
@@ -1,9 +1,40 @@
+import "../App/App.css";
 import "./Account.css";
+import { useState } from "react";
 
 function Account() {
+  const [password, setPassword] = useState("");
+
+  function handlePassChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Well, it worked! Account Edition");
+    setPassword("");
+  }
+
   return (
-    <div className="nav">
-      <a href="./">HomePage</a>
+    <div className="content-area">
+      <section className="account-area">
+        <h1>Account Info</h1>
+        <h2>Hello TBD-User</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="password">Change Password</label>
+          <br />
+          <input
+            type="password"
+            placeholder="Enter New Password"
+            name="password"
+            value={password}
+            onChange={handlePassChange}
+            required
+          />
+          <br />
+          <input type="submit" value="Update Password" />
+        </form>
+      </section>
     </div>
   );
 }
